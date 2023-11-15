513b3623277a0d1d94d4d2f21da04eba51bcf125
AnthonyScott <anthony.scott1005@gmail.com>
2023-11-15 04:36:43 -0500

    Add basic content to fill out page

--- src/app/bottom-square/bottom-square.component.ts
+++ src/app/bottom-square/bottom-square.component.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-bottom-square',
+  selector: 'bottom-square',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './bottom-square.component.html',
   styleUrl: './bottom-square.component.css'
 })
 export class BottomSquareComponent {
-
+  content: string = 'wootdoot';
 }
