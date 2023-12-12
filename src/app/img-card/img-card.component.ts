bd180a7b07d49a7229862cb1b2c900d4d38d15fe
AnthonyScott <anthony.scott1005@gmail.com>
2023-12-12 08:29:04 -0500

    Add card component

---
+++ src/app/img-card/img-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-img-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './img-card.component.html',
+  styleUrl: './img-card.component.css'
+})
+export class ImgCardComponent {
+
+}
